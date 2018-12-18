62fc258fc7406d9ef4c5740a804963706b341d79
Ines Montani <ines@ines.io>
2018-12-18 14:26:27 +0100

    wip

--- spacy/tests/regression/test_issue3012.py
+++ spacy/tests/regression/test_issue3012.py
@@ -1,18 +1,33 @@
-# coding: utf-8
+# coding: utf8
+from __future__ import unicode_literals
+
 import pytest
+
 from ...attrs import ENT_IOB, ENT_TYPE
 from ...tokens import Doc
+from ..util import get_doc
 
 
-@pytest.mark.xfail
-@pytest.mark.models('en')
-def test_issue3012(EN):
-    sentence = 'This is 10%.'
+# @pytest.mark.xfail
+def test_issue3012(en_vocab):
+    words = ["This", "is", "10", "%", "."]
+    tags = ["DT", "VBZ", "CD", "NN", "."]
+    pos = ["DET", "VERB", "NUM", "NOUN", "PUNCT"]
+    ents = [("10%", "PERCENT", 8, 11)]
+    doc = get_doc(en_vocab, words=words, tags=tags, pos=pos, ents=ents)
 
-    doc = EN(sentence)
+    ten = doc[2]
+    print(ten.text, ten.pos_, ten.tag_, ten.ent_type_)
 
+
+def p():
     ten = doc[2]  # 10
-    assert (ten.orth_, ten.orth_, ten.tag_, ten.ent_type_) == (10, 'NUM', 'CD', 'PERCENT')
+    assert (ten.orth_, ten.orth_, ten.tag_, ten.ent_type_) == (
+        10,
+        "NUM",
+        "CD",
+        "PERCENT",
+    )
 
     # now removing '10%' entity
     assert len(doc.ents) == 1
@@ -27,7 +42,12 @@ def test_issue3012(EN):
 
     # still making sure nothing changed on array() calls
     ten = doc[2]  # 10
-    assert (ten.orth_, ten.orth_, ten.tag_, ten.ent_type_) == (10, 'NUM', 'CD', 'PERCENT')
+    assert (ten.orth_, ten.orth_, ten.tag_, ten.ent_type_) == (
+        10,
+        "NUM",
+        "CD",
+        "PERCENT",
+    )
 
     # serializing then deserializing
     bytes = doc.to_bytes()
@@ -35,4 +55,9 @@ def test_issue3012(EN):
 
     assert len(doc2.ents) == 0
     ten2 = doc2[2]  # 10
-    assert (ten2.orth_, ten2.orth_, ten2.tag_, ten2.ent_type_) == (10, 'NUM', 'CD', 'PERCENT')
+    assert (ten2.orth_, ten2.orth_, ten2.tag_, ten2.ent_type_) == (
+        10,
+        "NUM",
+        "CD",
+        "PERCENT",
+    )
